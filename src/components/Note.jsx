0140f85911950d35c879602d001f75407bbd11bf
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-11-28 13:07:21 +0530

    Complete first part of Keeper App

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note() {
+    return <div className="note">
+        <h1>Here goes title</h1>
+        <p>Here goes content</p>
+    </div>;
+}
+
+export default Note;
