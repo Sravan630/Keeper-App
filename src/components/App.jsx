2c8f4a38a3aa892e3eeee7989231ae13a966e86d
Velpula,SravanKumar <sravan_velpula@optum.com>
2023-12-04 16:36:39 +0530

    Complete Part 2 of Keeper App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes";
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(noteElement => <Note 
+            key={noteElement.key}
+            title={noteElement.title}
+            content={noteElement.content}
+        />)}
         <Footer/>
         </div>;    
 }
